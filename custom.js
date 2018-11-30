61f678514202b1aee4af500c48ed26697c754fe2
Thilo Richter <trichter@ams-trichter1.local>
2018-11-30 16:13:38 +0100

    get plan

--- custom.js
+++ custom.js
@@ -1,51 +1,93 @@
-var optly = new OptimizelyAPI({
-                password: "test",
-                client_id: 12254990965
-            });
-
 'use strict';
 
+// var optly = new OptimizelyAPI({
+//                 password: "test",
+//                 client_id: 12254990965
+//             });
+//
+// const { Component, h, render } = window.preact;
+//
+// /** Example classful component */
+// class App extends Component {
+//   componentWillMount(){
+//     this.state = {accountData: {product_usages: [{projects: {}}]}};
+//   }
+//
+//   componentDidMount() {
+//     var accountInfo = optly.get("plan", function(res){
+//       this.setState({accountData: res});
+//       // console.log(this.state);
+//     }.bind(this));
+// 	}
+//
+// 	render(props, state) {
+//     return (
+//       h('div', null, "Test")
+//       // h('div', {id:'app'},
+// 			// 	//h(Header, { message: state.message }),
+// 			// 	h(UsageList, { projectData: this.state.accountData.product_usages[0].projects })
+// 			// )
+// 		);
+// 	}
+// }
+//
+// const UsageList = (props) => {
+//   console.log(props.projectData);
+//   var projectElements = [];
+//
+//   for (project in props.projectData){
+//     projectElements.push(
+//       h('div', null, props.projectData[project])
+//     );
+//   }
+//
+//   return h('div', null, projectElements);
+// };
+//
+// /** Instead of JSX, use: h(type, props, ...children) */
+// // class UsageList extends Component {
+// // 	render() {
+// //     console.log(props);
+// //     const items = [1,2,3,4,5].map( (item) => (
+// // 			h('li', {id:item}, 'Item '+item)
+// // 		));
+// // 		return (
+// // 			h('main', null,
+// // 				h('ul', null, items)
+// // 			)
+// // 		);
+// // 	}
+// // }
+//
+// /** Components can just be pure functions */
+// const Header = (props) => {
+// 	return h('header', null,
+// 		h('h1', null, 'App'),
+// 		props.message && h('h2', null, props.message)
+// 	);
+// };
+//
+// render(h(App), document.body);
+
 const { Component, h, render } = window.preact;
+
 
 /** Example classful component */
 class App extends Component {
 	componentDidMount() {
-    var accountInfo = optly.get("plan", function(res){
-      console.log(res);
-      this.setState({ projectData: res });
-    }).bind(this);
+		this.setState({ message:'Hello!' });
 	}
 
 	render(props, state) {
-		return (
-			h('div', {id:'app'},
-				//h(Header, { message: state.message }),
-				h(Main)
-			)
-		);
+    console.log("rendering");
+    return
+      h(
+          'div',
+          { id: 'foo' },
+          h('span', null, 'Hello!')
+      )
 	}
 }
 
-/** Components can just be pure functions */
-const Header = (props) => {
-	return h('header', null,
-		h('h1', null, 'App'),
-		props.message && h('h2', null, props.message)
-	);
-};
-
-/** Instead of JSX, use: h(type, props, ...children) */
-class Main extends Component {
-	render() {
-		const items = [1,2,3,4,5].map( (item) => (
-			h('li', {id:item}, 'Item '+item)
-		));
-		return (
-			h('main', null,
-				h('ul', null, items)
-			)
-		);
-	}
-}
 
 render(h(App), document.body);
