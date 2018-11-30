0369d8af27bfb312471a98fc36c802b91ab2634e
Thilo Richter <trichter@ams-trichter1.local>
2018-11-30 14:26:31 +0100

    get plan

--- custom.js
+++ custom.js
@@ -2,3 +2,7 @@ var optly = new OptimizelyAPI({
                 password: "test",
                 client_id: 12254990965
             });
+
+var accountInfo = optly.get("plan",null,function(res){
+  console.log(res);
+});
